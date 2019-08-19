2208088a61b5e7e7763d74c27ebfadaf7df10c01
Majed Khalil <majedkhalil17@gmail.com>
2019-08-19 10:25:37 -0700

    working

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name,) VALUES ("Halal Chicken Sandwhich");
+INSERT INTO burgers (burger_name,) VALUES ("Halal Shawarma Sandwhich");
+INSERT INTO burgers (burger_name,) VALUES ("Halal Lamb Burger with Garlic Sauce");
+INSERT INTO burgers (burger_name,) VALUES ("Halal Falafel Sandwhich with Tahin Sauce");
+
+
